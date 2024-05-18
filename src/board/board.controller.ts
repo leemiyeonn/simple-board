3f6a547e406ab2d567da85c4e135c503055dc99a
leemiyeon <lemingul7@gmail.com>
2024-05-18 17:15:41 +0900

    feat: add board

---
+++ src/board/board.controller.ts
@@ -0,0 +1,40 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Injectable,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { BoardService } from './board.service';
+
+@Controller('board')
+export class BoardController {
+  constructor(private readonly boardService: BoardService) {}
+  @Get()
+  findAll() {
+    return this.boardService.findAll();
+  }
+
+  @Get(':id')
+  find(@Param('id') id: number) {
+    return `find id: ${id}`;
+  }
+
+  @Post()
+  create(@Body() data) {
+    return 'create';
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() data) {
+    return `update id: ${id}`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return `remove id: ${id}`;
+  }
+}
